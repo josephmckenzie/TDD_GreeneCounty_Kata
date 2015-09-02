40c163c25cf626dac6c768790ce347ac66b64572
josephmckenzie <josephmckenzieaz@yahoo.com>
2015-09-02 18:01:40 -0400

    updated gc_kata to change all values equalt to 3 and 5 to Greenecounty

--- GC_kata.rb
+++ GC_kata.rb
@@ -1,7 +1,9 @@
 def greene_county
 array=[*(1..100)]
 array.each_with_index do |value,index|
-if value%3==0
+if value%3==0 && value%5==0
+array[index]= "Greene County"
+elsif value%3==0
 array[index]="greene"
 elsif value%5==0
 array[index]="county"
